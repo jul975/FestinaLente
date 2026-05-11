9cc445bf8a76aaf702b28e4dc966a514217a98a2
Jules <juleslowette@gmail.com>
2026-05-11 18:15:19 +0200

    tests

--- FestinaLente/regimes/deb/__init__.py
+++ FestinaLente/regimes/deb/__init__.py
@@ -6,17 +6,7 @@ Theta package:
 
 Defines regime-level ratios:
 
-    mean cell capacity $    git rm --cached docsZ.zip
-    git commit -m "Remove docsZ.zip"
-    git push\kappa$
-    mean cell inflow $\phi$
-    spatial inequality $\mathcal{S}$
-    harvest ratio $\rho_h$
-    reserve ratio $\rho_e$
-    maintenance ratio $\rho_m$
-    movement ratio $\rho_{\mathrm{move}}$
-    reproduction threshold ratio $\rho_r$
-    reproduction burden ratio $\rho_b$
+
 
 
 """
